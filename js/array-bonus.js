6a091f6f8dafce02497302f1e3a42da884910852
MKovar <mkovar@mkovar.local>
2018-06-22 13:42:06 -0500

    js stuff

--- js/array-bonus.js
+++ js/array-bonus.js
@@ -61,7 +61,11 @@ toggleStrUsername(usernameString);
 //index of !== toLowerCase??
 
 // BONUS 2:
-// Write a program that takes a given string appended with a number at the end. The program will check the length of the string to verify the string is equal to the number appended. If the number appended equals the string length, output "Yes". if the number appended does not equal the string length, output "No".
+// Write a program that takes a given string appended with a number at the end.
+// The program will check the length of the string to verify the string is equal to the number appended.
+// If the number appended equals the string length, output "Yes".
+// if the number appended does not equal the string length, output "No".
+
 // Example Input: "geek5"
 // Example Output: "Yes"
 // Example Input: "codingchallenge25"
