62a0854ea5ce181d66f803d7f91dd0f9d270ec6b
MKovar <mkovar@mkovar.local>
2018-06-29 13:55:42 -0500

    mouse event exercise

--- js/play.js
+++ js/play.js
@@ -7,13 +7,13 @@
 
 
 
-$(function(){
-
-    $('#playBtn').click(function() {
-    $('#playOutput').text($('#playInput').val());
-    });
-
-    $('#playBtn').css('background-color', 'blue');
+// $(function(){
+    //
+    // $('#playBtn').click(function() {
+    // $('#playOutput').text($('#playInput').val());
+    // });
+    //
+    // $('#playBtn').css('background-color', 'blue');
 
     //add event listener is replaced by on here.
     // $('.important').on('mouseover', function() {
@@ -21,27 +21,57 @@ $(function(){
     //
     // });
 
-    $('.mainContainer').css('font-family', 'helvetica');
-    $('#header').css('text-align', 'center');
+    // $('.mainContainer').css('font-family', 'helvetica');
+    // $('#header').css('text-align', 'center');
+    //
+    // // alert($('#snowboard').text());
+    // // alert($('#double').text());
+    //
+    // $('#double').click(function() {
+    //     $('#playOutput').text($('#playInput').val());
+    // });
+    //
+    // $('.codeup').css('border', '3px solid red');
+    // $('p').css('width', '500px');
+    //
+    // // can chain as many as we need, like the li element below.
+    // $('li').css('font-size', '20px').css('background-color', 'blue');
+    // $('h1, p').css('background-color', 'yellow');
+    // $('h3').css('text-align', 'center').css('background-color', 'aqua');
+    //
+    // alert($('h1').text());
 
-    // alert($('#snowboard').text());
-    // alert($('#double').text());
+    function changeH1Color () {
+        $('h1').css('background-color', 'yellow');
 
-    $('#double').click(function() {
-        $('#playOutput').text($('#playInput').val());
+    }
+
+    $('h1').click(changeH1Color);
+
+    function pClickChange() {
+        $('p').css('font-size', '18px');
+    }
+
+    $('p').dblclick(pClickChange);
+
+    // function listHoverColor() {
+    //     $('li').css('color', 'red');
+    // }, function () {
+    //     $('li').css('color', 'black');
+    // };
+    //
+    // $('li').hover(listHoverColor);
+
+    $('li').hover(function(){
+        $('li').css('color', 'red');
+
+    }, function() {
+
+        $('li').css('color', 'black');
     });
 
-    $('.codeup').css('border', '3px solid red');
-    $('p').css('width', '500px');
 
-    // can chain as many as we need, like the li element below.
-    $('li').css('font-size', '20px').css('background-color', 'blue');
-    $('h1, p').css('background-color', 'yellow');
-    $('h3').css('text-align', 'center').css('background-color', 'aqua');
-
-    alert($('h1').text());
-
-})
+// })
 
 
 
