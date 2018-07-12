1bffb938fc55910d34eaf6c4ac7055c66b0f9d76
MKovar <mkovar@mkovar.attlocal.net>
2018-07-11 19:40:00 -0500

    weather map project functionality complete

--- js/weather_map_script.js
+++ js/weather_map_script.js
@@ -1,111 +1,104 @@
 "use strict";
-    (function() {
 
-        // TO DOs:
-        // practice writing console.logs of various parts of the OpenWeatherMap object...
-        // console.log the humidity of the 2nd day - done
-        // console.log the humidity of all the days (maybe use .forEach?) - done
-        // console.log the temperature of the 3rd day in celsius - done
-        // console.log the temperature of the 3rd day in Fahrenheit - done
-        // console.log all types of information you will ultimately display for a single day in the exercise - done
-        // console.log all type of information you will ultimately display for all days in the 3-day forecast divs -
+    // =================================     ** global vars  **    ========================================= \\
 
-        //AJAX get request from weather map API
-        $.get("http://api.openweathermap.org/data/2.5/forecast", {
-            APPID: "e12d834255482e959a94744605064c71",
-            id: 4726206,
-            units: "imperial"
-        }).done(function(data){
-            //c-log data from request to confirm working correctly
+    var lat = 29.42;
+    var lng = -98.49;
+
+// =================================     ** function for AJAX updates **      ================================= \\
+    function updateWeatherCardInfo(lat, lng){
+        $.get("http://api.openweathermap.org/data/2.5/forecast?appid=e12d834255482e959a94744605064c71&units=imperial&lat="
+            + lat + "&lon=" + lng).done(function(data){
             console.log(data);
+            $('#city-name').html(data.city.name);
 
+            //refactor building cards by using a loop of some kind. DRY:(
             //weather card info for day 1
-            $('#high-low-temp1').append("<p>High & Low: " + roundTemperatures(data.list[0].main.temp_max)+ "&#8457 / "+ roundTemperatures(data.list[0].main.temp_min) + "&#8457</p>");
-            $('#current-temp1').append("<p>Currently: " + roundTemperatures(data.list[0].main.temp) + "&#8457</p>");
-            $('#humidity1').append("<p>Humidity: " + data.list[0].main.humidity + "%</p>");
-            $('#windspeed1').append("<p>Wind Speed: " + data.list[0].wind.speed + "mph</p>");
+            $('#high-low-temp1').html("<p>High & Low: " + roundTemperatures(data.list[0].main.temp_max)+ "&#8457 / "+ roundTemperatures(data.list[0].main.temp_min) + "&#8457</p>");
+            $('#current-temp1').html("<p>Currently: " + roundTemperatures(data.list[0].main.temp) + "&#8457</p>");
+            $('#humidity1').html("<p>Humidity: " + data.list[0].main.humidity + "%</p>");
+            $('#windspeed1').html("<p>Wind Speed: " + data.list[0].wind.speed + "mph</p>");
 
             //weather card info for day 2
-            $('#high-low-temp2').append("<p>High & Low: " + roundTemperatures(data.list[8].main.temp_max) + "&#8457 / "+ roundTemperatures(data.list[8].main.temp_min) + "&#8457</p>");
-            $('#current-temp2').append("<p>Currently: " + roundTemperatures(data.list[8].main.temp) + "&#8457</p>");
-            $('#humidity2').append("<p>Humidity: " + data.list[8].main.humidity + "%</p>");
-            $('#windspeed2').append("<p>Wind Speed: " + data.list[8].wind.speed + "mph</p>");
+            $('#high-low-temp2').html("<p>High & Low: " + roundTemperatures(data.list[8].main.temp_max) + "&#8457 / "+ roundTemperatures(data.list[8].main.temp_min) + "&#8457</p>");
+            $('#current-temp2').html("<p>Currently: " + roundTemperatures(data.list[8].main.temp) + "&#8457</p>");
+            $('#humidity2').html("<p>Humidity: " + data.list[8].main.humidity + "%</p>");
+            $('#windspeed2').html("<p>Wind Speed: " + data.list[8].wind.speed + "mph</p>");
 
             //weather card info for day 3
-            $('#high-low-temp3').append("<p>High & Low: " + roundTemperatures(data.list[16].main.temp_max) + "&#8457 / "+ roundTemperatures(data.list[16].main.temp_min) + "&#8457</p>");
-            $('#current-temp3').append("<p>Currently: " + roundTemperatures(data.list[16].main.temp) + "&#8457</p>");
-            $('#humidity3').append("<p>Humidity: " + data.list[16].main.humidity + "%</p>");
-            $('#windspeed3').append("<p>Wind Speed: " + data.list[16].wind.speed + "mph</p>");
+            $('#high-low-temp3').html("<p>High & Low: " + roundTemperatures(data.list[16].main.temp_max) + "&#8457 / "+ roundTemperatures(data.list[16].main.temp_min) + "&#8457</p>");
+            $('#current-temp3').html("<p>Currently: " + roundTemperatures(data.list[16].main.temp) + "&#8457</p>");
+            $('#humidity3').html("<p>Humidity: " + data.list[16].main.humidity + "%</p>");
+            $('#windspeed3').html("<p>Wind Speed: " + data.list[16].wind.speed + "mph</p>");
 
+        //    cards need to be hidden
+        //    once toggle button is CLICKED, reveal and space accordingly
+        //    html will be added to ids = day-four-weather-info & day-five-weather-info
+            $('#high-low-temp4').html("<p>High & Low: " + roundTemperatures(data.list[24].main.temp_max) + "&#8457 / "+ roundTemperatures(data.list[24].main.temp_min) + "&#8457</p>");
+            $('#current-temp4').html("<p>Currently: " + roundTemperatures(data.list[24].main.temp) + "&#8457</p>");
+            $('#humidity4').html("<p>Humidity: " + data.list[24].main.humidity + "%</p>");
+            $('#windspeed4').html("<p>Wind Speed: " + data.list[24].wind.speed + "mph</p>");
 
+            $('#high-low-temp5').html("<p>High & Low: " + roundTemperatures(data.list[24].main.temp_max) + "&#8457 / "+ roundTemperatures(data.list[24].main.temp_min) + "&#8457</p>");
+            $('#current-temp5').html("<p>Currently: " + roundTemperatures(data.list[24].main.temp) + "&#8457</p>");
+            $('#humidity5').html("<p>Humidity: " + data.list[24].main.humidity + "%</p>");
+            $('#windspeed5').html("<p>Wind Speed: " + data.list[24].wind.speed + "mph</p>");
 
         });
+    }
 
-        //function to build weather card when called
-        // would I just pass this function the data.list index for it work properly?
-        // function generateWeatherCardInfo(day){
-        //     var html = "<div>";
-        //     html+= "<p>" + day.main.temp_max.join("/") + day.main.temp_min + "</p>";
-        //     html+= "<p> Current Temperature: " + day.main.temp + "</p>";
-        //     html+= "<p> Humidity: " + day.main.humidity + "</p>";
-        //     html+= "<p> Wind Speed: " + day.wind.speed+"MPH</p>";
-        //     var html = "</div>";
-        //     //do i need append(html) here?
-        // }
+    updateWeatherCardInfo(lat, lng);
 
-        //create a math.round function to use for weather card temperatures. Does it have to be local?
+
+    // =================================     ** google map functionality **      ================================= \\
+
+        function initMap() {
+            var mapCanvas = document.getElementById('map-canvas');
+            var mapOptions = {
+                zoom: 13,
+                center: {
+                    lat: lat,
+                    lng: lng
+                }
+            };
+
+            var map = new google.maps.Map(mapCanvas, mapOptions);
+            var marker = new google.maps.Marker({
+                draggable: true,
+                position: {
+                    lat: lat,
+                    lng: lng
+                },
+                map: map
+            });
+
+
+            // event listener to store updated lat and lng when marker is dragged, info passed to weather card updating function
+            google.maps.event.addListener(marker, 'dragend', function(e){
+                var lat = marker.getPosition().lat();
+                var lng = marker.getPosition().lng();
+
+                updateWeatherCardInfo(lat, lng);
+            });
+
+        }
+// =================================     ** additional function features **      ================================= \\
+
         function roundTemperatures(num){
             return Math.round(num);
         }
 
 
 
+        $('#display-5-day-forecast').on('click', function(){
+            $('div .card:hidden').show();
+        });
+
+        $('#display-3-day-forecast').on('click', function(){
+            $('#day-four, #day-five').hide();
+        });
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-        // ** console.log the humidity of the 2nd day ** \\
-        // console.log(data.list[8].main.humidity);
-        // console.log(data.list[16].main.humidity);
-
-        // ** console.log the humidity of all the days ** \\
-        // data.list.forEach(function(day) {
-        //     console.log(day.main.humidity);
-        // });
-
-        // ** console.log the temperature of the 3rd day in celsius ** \\
-        //could use function to convert temp, but also read in documentation
-        // switching "units" also solves this.
-        // console.log(data.list[24].main.temp);
-
-        // ** console.log all types of information you will ultimately display for a single day in the exercise ** \\
-        // data.list.forEach(function(item){
-        //     console.log(item.main.temp);
-        // })
-        // console.log(data.list[0].main.humidity, data.list[0].main.temp, data.list[0].main.pressure);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // ** IFFE END ** \\
-    })();
